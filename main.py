8a69906eba02cf7a601c65ebe487d44f9a4fc828
Dimitri Volkmann <d4e5v8@gmail.com>
2023-06-07 00:33:52 -0700

    First real prototype

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-
 import requests
-import json5
+import json
+import time
 from datetime import datetime
+from dateutil.tz import tzutc
+
 
 def get_orders(auth_token, start_date, end_date):
     url = "https://api.squarespace.com/1.0/commerce/orders"
 
-    # Ensure dates are in ISO 8601 format
-    if not isinstance(start_date, str) or not isinstance(end_date, str):
-        start_date = datetime.strptime(start_date, "%Y-%m-%d").isoformat()
-        end_date = datetime.strptime(end_date, "%Y-%m-%d").isoformat()
+    # Ensure dates are in ISO 8601 UTC format
+    print('100|OCT orders... here we go')
+
+    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    start_date = start_date.replace(tzinfo=tzutc()).isoformat()
+    start_date = start_date.replace('00:00:00+00:00', '08:00:00Z')
+
+    end_date = datetime.strptime(end_date, "%Y-%m-%d")
+    end_date = end_date.replace(tzinfo=tzutc()).isoformat()
+    end_date = end_date.replace('+00:00', 'Z')
+
+#    print(start_date)
+#    print(end_date)
 
     headers = {
         'Authorization': f'Bearer {auth_token}',
@@ -22,12 +33,21 @@ def get_orders(auth_token, start_date, end_date):
 
     all_orders = []
     while True:
-        response = requests.get(url, headers=headers, params=params)
+        response = requests.get(url, headers=headers, params=params)#
+#        print(url)
+#        print(headers)
+#        print(params)
+#        print('.')
         if response.status_code == 200:
-            data = json5.loads(response.content.decode('utf-8'))
+            data = json.loads(response.content.decode('utf-8'))
             all_orders.extend(data['result'])
-            if 'nextPageCursor' in data['pagination']:
-                params['cursor'] = data['pagination']['nextPageCursor']
+#            print(data['pagination']['hasNextPage'])
+            if data['pagination']['hasNextPage']:
+                time.sleep(1)
+                params = {
+                    'cursor' : data['pagination']['nextPageCursor']
+                }
+#                print(params)
             else:
                 break
         else:
@@ -37,8 +57,51 @@ def get_orders(auth_token, start_date, end_date):
 
 
 # usage
-auth_token = '22abd801-7679-47f2-8616-13fba006a243'
-start_date = '2023-05-26'  # Change to your desired start date
-end_date = '2023-06-06'  # Change to your desired end date
+auth_token = '0ed8689f-0649-4436-998f-3077d3c3f857'
+start_date = '2023-05-01'  # Change to your desired start date
+end_date = '2023-06-15'  # Change to your desired end date
 orders = get_orders(auth_token, start_date, end_date)
-print(orders)
+
+nb_of_orders_retrieved = len(orders)
+# print(type(nb_of_orders_retrieved))
+
+
+# Now we got the list of all orders over the period, need to split into two lists:
+# product ID 644ab8918a97511ab4375314 - Car Display Ticket
+# product ID 644abffb0be53a6986136cd8 - Guest Ticket
+carDisplayTicket = '644ab8918a97511ab4375314'
+guestTicket = '644abffb0be53a6986136cd8'
+
+display_cars_list = ''
+guests_list = ''
+
+pt = 0
+while (pt < nb_of_orders_retrieved):
+    line_items = orders[pt]['lineItems']
+    nb_lines = len(line_items)
+    pt_line = 0
+    while pt_line < nb_lines:
+#        print('line', pt_line)
+        line_item = line_items[pt_line]
+        if line_item['productId'] == guestTicket or line_item['productId'] == carDisplayTicket:
+            print(pt, ': -->> Order: ', orders[pt]['orderNumber'])
+            if line_item['productId'] == guestTicket:
+                print('Nb of tickets: ',line_item['quantity'])
+                print('Guest: ', line_item['customizations'][0]['value'])
+            elif line_item['productId'] == carDisplayTicket:
+                print('Nb of tickets: ', line_item['quantity'])
+                print('Guest: ', line_item['customizations'][0]['value'])
+                print('Year: ', line_item['customizations'][4]['value'])
+                print('Make: ', line_item['customizations'][5]['value'])
+                print('Model: ', line_item['customizations'][6]['value'])
+                print('Color: ', line_item['customizations'][7]['value'])
+            print("------")
+        single_line_item = line_items[pt_line]
+        pt_line = pt_line + 1
+    pt = pt+1
+
+
+
+
+
+
